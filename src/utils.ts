9921b87003ef5443a1b4f5b5eacc7a462a77afd4
minmoong <kim774634@gmail.com>
2023-12-14 22:44:58 +0900

    axios 요청시 오류 출력

--- src/utils.ts
+++ src/utils.ts
@@ -6,8 +6,11 @@ async function AxiosAndCheck(url: string, config?: AxiosRequestConfig) {
     const res = await axios.get(url, config);
     return res;
   } catch (error) {
+    console.error(error);
     if (axios.isAxiosError(error)) {
-      throw new Error(`${error.config?.url}: Axios HTTP 요청에 실패하였습니다.`);
+      throw new Error(
+        `${error.config?.url}: Axios HTTP 요청에 실패하였습니다.`
+      );
     }
     throw error;
   }
